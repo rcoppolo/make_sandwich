1e1e4ec0ca84f9e8cdd4c7475231570ced75ea9b
Ryan Coppolo <r@coppolo.com>
2012-04-12 09:30:38 -0700

    Bundles, sets up testing.

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,16 @@ module MakeSandwich
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+		config.generators do |g|
+			g.test_framework :rspec,
+				:fixtures => true,
+				:view_specs => false,
+				:helper_specs => false,
+				:routing_specs => false,
+				:controller_specs => true,
+				:request_spec => true
+			g.fixture_replacement :factory_gril, :dir => 'spec/factories'
+		end
   end
 end
